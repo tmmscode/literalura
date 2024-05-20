882b713bb698ff921f8fe31e41e3a0d4197bdbd5
tmmscode <thiago.meloms@gmail.com>
2024-05-20 19:49:19 -0300

    database connection

--- src/main/java/tmmscode/literalura/LiteraluraApplication.java
+++ src/main/java/tmmscode/literalura/LiteraluraApplication.java
@@ -1,13 +1,20 @@
 package tmmscode.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tmmscode.literalura.main.Main;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.start();
+	}
 }
---
+++ src/main/java/tmmscode/literalura/main/Main.java
@@ -0,0 +1,8 @@
+package tmmscode.literalura.main;
+
+public class Main {
+
+    public void start() {
+        System.out.println("Application is running...!");
+    }
+}
